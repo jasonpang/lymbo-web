ddec6cb831d042d6d7b1d252313ab3586db20a5c
Jason Pang <jasonpang2011@gmail.com>
2013-10-09 02:37:38 -0700

    Added basic landing page; no functionality

--- lymbo/views.py
+++ lymbo/views.py
@@ -1,6 +1,7 @@
 from pyramid.view import view_config
 
 
-@view_config(route_name='home', renderer='mytemplate.mako')
+@view_config(route_name='home', renderer='landing_page.mako')
 def my_view(request):
+    print("View got called.")
     return {'project': 'Lymbo'}
--- runapp.py
+++ runapp.py
@@ -7,4 +7,5 @@ if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app = loadapp('config:development.ini', relative_to='.')
 
+    print("About to serve app.")
     serve(app, host='0.0.0.0', port=port)
